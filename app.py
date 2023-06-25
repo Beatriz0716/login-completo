311dc737472315e253cf2a736b8d450dd4e90345
Beatriz Santos <beatriz1603santos@outlook.com>
2023-06-25 19:17:30 -0300

    criando banco de dados

--- app.py
+++ app.py
@@ -1,11 +1,71 @@
 import customtkinter as ctk
-from tkinter import PhotoImage
+from tkinter import *
+import sqlite3
+from tkinter import messagebox
 
-class App(ctk.CTk):
+class BackEnd():
+    def connect_db(self):
+        self.conn = sqlite3.connect('sistemaCadastro.db')
+        self.cursor = self.conn.cursor()
+        print('Banco de dados conectado')
+
+    def disconnect_db(self):
+        self.conn.close()
+        print('Banco de dados desconectado')
+
+    def create_table(self):
+        self.connect_db()
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL,
+                email TEXT NOT NULL,
+                password TEXT NOT NULL,
+                cPassword TEXT NOT NULL
+            );
+        ''')
+        self.conn.commit()
+        print('Tabela criada com sucesso!')
+        self.disconnect_db()
+
+    def register_user(self):
+        self.username_register = self.username_register_entry.get()
+        self.email_register = self.email_register_entry.get()
+        self.password_register = self.password_register_entry.get()
+        self.comfirm_register = self.confirm_register_entry.get()
+
+        self.connect_db()
+
+        self.cursor.execute('''
+            INSERT INTO users (username, email, password, cPassword)
+            VALUES (?, ?, ?, ?)''', (self.username_register, self.email_register, self.password_register, self.comfirm_register))
+        
+        try:
+            if(self.username_register == '' or self.email_register == '' or self.password_register == '' or self.comfirm_register == ''):
+                messagebox.showerror(title='Sistema de login', message='Preencha todos os campos!')
+            elif(len(self.username_register) < 4):
+                messagebox.showwarning(title='Sistema de login', message='O nome deve ter pelo menos 4 caracteres.')
+            elif(len(self.email_register) < 12):
+                messagebox.showwarning(title='Sistema de login', message='O e-mail deve ter pelo menos 12 caracteres.')
+            elif(len(self.password_register) < 4):
+                messagebox.showwarning(title='Sistema de login', message='A senha deve ter pelo menos 4 caracteres.')
+            elif(self.password_register != self.comfirm_register):
+                messagebox.showerror(title='Sistema de login', message='ERRO!!!\nAs senhas não são iguais.')
+            else:
+                self.conn.commit()
+                messagebox.showinfo(title='Sistema de login', message=f'{self.username_register}\nOs seus dados foram cadastrados com sucesso!')
+        except:
+            messagebox.showerror(title='Sistema de login', message='Erro no prosessamento do seu cadastro!\nTente novamente!')
+
+
+
+class App(ctk.CTk, BackEnd):
     def __init__(self):
         super().__init__()
         self.configuracoes_janela_principal()
         self.tela_login()
+        self.create_table()
 
 
     #configurando tela principal
@@ -78,12 +138,21 @@ class App(ctk.CTk):
         self.ckeckboxPass_register_entry = ctk.CTkCheckBox(self.frame_register, text='Mostrar senha', font=('Century Gothic ', 14, 'bold'), corner_radius=20)
         self.ckeckboxPass_register_entry.grid(row=5, column=0, padx=10, pady=10)
 
-        self.btn_save = ctk.CTkButton(self.frame_register, width=300, text='fazer cadastro'.upper(), font=('Century Gothic ', 16, 'bold'), corner_radius=15, fg_color='green', hover_color='#005900')
+        self.btn_save = ctk.CTkButton(self.frame_register, width=300, text='fazer cadastro'.upper(), font=('Century Gothic ', 16, 'bold'), corner_radius=15, fg_color='green', hover_color='#005900', command=self.register_user)
         self.btn_save.grid(row=6, column=0, padx=10, pady=5)
 
         self.btn_back = ctk.CTkButton(self.frame_register, width=300, text='voltar ao login'.upper(), font=('Century Gothic ', 16, 'bold'), corner_radius=15, fg_color='gray', hover_color='#303030', command=self.tela_login)
         self.btn_back.grid(row=7, column=0, padx=10, pady=15)
 
+    def clean_register(self):
+        self.username_register_entry.delete(0, END)
+        self.email_register_entry.delete(0, END)
+        self.password_register_entry.delete(0, END)
+        self.confirm_register_entry.delete(0, END)
+
+    def clean_login(self):
+        self.username_login_entry.delete(0, END)
+        self.password_login_entry.delete(0, END)
 
 if __name__=='__main__':
     app = App()
